5e2e671f99a0e2491a8c6b2fe644dd69c3033a40
Albert Goncalves <goncalves.albert@gmail.com>
2020-10-29 00:18:58 -0400

    init parser

--- 03_asm/src/main.c
+++ 03_asm/src/main.c
@@ -6,6 +6,7 @@
 typedef FILE File;
 
 typedef uint8_t  u8;
+typedef uint16_t u16;
 typedef uint32_t u32;
 
 typedef int32_t i32;
@@ -13,6 +14,11 @@ typedef int32_t i32;
 #define COUNT_CHARS  512
 #define COUNT_BUFFER 24
 #define COUNT_TOKENS 128
+
+// typedef enum {
+//     FALSE = 0,
+//     TRUE,
+// } Bool;
 
 typedef enum {
     TOKEN_MAJOR_VERSION,
@@ -23,18 +29,25 @@ typedef enum {
 
 typedef struct {
     union {
-        i32 number;
+        u32 number;
     };
     u32      line;
     TokenTag tag;
 } Token;
 
 typedef struct {
-    u32   file_size;
-    char  chars[COUNT_CHARS];
-    char  buffer[COUNT_BUFFER];
-    u32   token_index;
-    Token tokens[COUNT_TOKENS];
+    u16 major_version;
+    u16 minor_version;
+} Program;
+
+typedef struct {
+    u32     file_size;
+    char    chars[COUNT_CHARS];
+    char    buffer[COUNT_BUFFER];
+    u32     token_index;
+    u32     token_count;
+    Token   tokens[COUNT_TOKENS];
+    Program program;
 } Memory;
 
 static void set_file_to_chars(Memory* memory, const char* filename) {
@@ -59,20 +72,20 @@ static void set_file_to_chars(Memory* memory, const char* filename) {
 }
 
 static Token* alloc_token(Memory* memory) {
-    if (COUNT_TOKENS <= memory->token_index) {
+    if (COUNT_TOKENS <= memory->token_count) {
         fprintf(stderr, "[ERROR] Unable to allocate new token\n");
         exit(EXIT_FAILURE);
     }
-    return &memory->tokens[memory->token_index++];
+    return &memory->tokens[memory->token_count++];
 }
 
-static i32 get_decimal(const char* decimal) {
-    i32 result = 0;
+static u32 get_decimal(const char* decimal) {
+    u32 result = 0;
     while (*decimal) {
         u8  digit = (u8)(*(decimal++));
-        i32 value = 0;
+        u32 value = 0;
         if (('0' <= digit) && (digit <= '9')) {
-            value = (i32)(digit - '0');
+            value = (u32)(digit - '0');
         } else {
             fprintf(stderr, "[ERROR] Unable to parse decimal\n");
             exit(EXIT_FAILURE);
@@ -82,17 +95,17 @@ static i32 get_decimal(const char* decimal) {
     return result;
 }
 
-static i32 get_hex(const char* hex) {
-    i32 result = 0;
+static u32 get_hex(const char* hex) {
+    u32 result = 0;
     while (*hex) {
         u8  digit = (u8)(*(hex++));
-        i32 value = 0;
+        u32 value = 0;
         if (('0' <= digit) && (digit <= '9')) {
-            value = (i32)(digit - '0');
+            value = (u32)(digit - '0');
         } else if (('a' <= digit) && (digit <= 'f')) {
-            value = (i32)((digit - 'a') + 10);
+            value = (u32)((digit - 'a') + 10);
         } else if (('A' <= digit) && (digit <= 'F')) {
-            value = (i32)((digit - 'A') + 10);
+            value = (u32)((digit - 'A') + 10);
         } else {
             fprintf(stderr, "[ERROR] Unable to parse hex\n");
             exit(EXIT_FAILURE);
@@ -100,17 +113,6 @@ static i32 get_hex(const char* hex) {
         result = (result << 4) + value;
     }
     return result;
-}
-
-static i32 get_number(const char* number, u8 length) {
-    if ((1 < length) && (number[1] == 'x')) {
-        if (length == 2) {
-            fprintf(stderr, "[ERROR] Unable to parse hex\n");
-            exit(EXIT_FAILURE);
-        }
-        return get_hex(&number[2]);
-    }
-    return get_decimal(number);
 }
 
 static void set_tokens(Memory* memory) {
@@ -167,7 +169,15 @@ static void set_tokens(Memory* memory) {
             buffer[k] = '\0';
             if (('0' <= buffer[0]) && (buffer[0] <= '9')) {
                 token->tag = TOKEN_NUMBER;
-                token->number = get_number(buffer, k);
+                if ((1 < k) && (buffer[1] == 'x')) {
+                    if (k == 2) {
+                        fprintf(stderr, "[ERROR] Unable to parse hex\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    token->number = get_hex(&buffer[2]);
+                } else {
+                    token->number = get_decimal(buffer);
+                }
             } else if (!strcmp(buffer, "minor_version")) {
                 token->tag = TOKEN_MINOR_VERSION;
             } else if (!strcmp(buffer, "major_version")) {
@@ -178,32 +188,90 @@ static void set_tokens(Memory* memory) {
     }
 }
 
-#define TOKEN_FMT_LINE "%-4u"
+#define TOKEN_FMT_LINE "ln. %-4u"
 
-static void print_tokens(Memory* memory) {
-    for (u32 i = 0; i < memory->token_index; ++i) {
-        Token token = memory->tokens[i];
-        switch (token.tag) {
-        case TOKEN_MAJOR_VERSION: {
-            printf(TOKEN_FMT_LINE "TOKEN_MAJOR_VERSION\n", token.line);
-            break;
-        }
-        case TOKEN_MINOR_VERSION: {
-            printf(TOKEN_FMT_LINE "TOKEN_MINOR_VERSION\n", token.line);
-            break;
-        }
-        case TOKEN_NUMBER: {
-            printf(TOKEN_FMT_LINE "TOKEN_NUMBER %d\n",
-                   token.line,
-                   token.number);
-            break;
-        }
-        case TOKEN_MINUS: {
-            printf(TOKEN_FMT_LINE "TOKEN_MINUS\n", token.line);
-            break;
-        }
-        }
+static void fprint_token(File* file, Token token) {
+    switch (token.tag) {
+    case TOKEN_MAJOR_VERSION: {
+        fprintf(file, TOKEN_FMT_LINE "TOKEN_MAJOR_VERSION\n", token.line);
+        break;
     }
+    case TOKEN_MINOR_VERSION: {
+        fprintf(file, TOKEN_FMT_LINE "TOKEN_MINOR_VERSION\n", token.line);
+        break;
+    }
+    case TOKEN_NUMBER: {
+        fprintf(file,
+                TOKEN_FMT_LINE "TOKEN_NUMBER %u\n",
+                token.line,
+                token.number);
+        break;
+    }
+    case TOKEN_MINUS: {
+        fprintf(file, TOKEN_FMT_LINE "TOKEN_MINUS\n", token.line);
+        break;
+    }
+    }
+}
+
+static Token pop_token(Memory* memory) {
+    if (COUNT_TOKENS <= memory->token_index) {
+        fprintf(stderr, "[ERROR] Unable to pop token\n");
+        exit(EXIT_FAILURE);
+    }
+    return memory->tokens[memory->token_index++];
+}
+
+#define UNEXPECTED_TOKEN(function, token)                             \
+    {                                                                 \
+        fprintf(stderr, "[ERROR] `" function "` unexpected token\n"); \
+        fprint_token(stderr, token);                                  \
+        exit(EXIT_FAILURE);                                           \
+    }
+
+static u32 get_unsigned(Memory* memory) {
+    Token token = pop_token(memory);
+    if (token.tag != TOKEN_NUMBER) {
+        UNEXPECTED_TOKEN("get_unsigned", token);
+    }
+    return token.number;
+}
+
+// static i32 get_signed(Memory* memory) {
+//     Token token = pop_token(memory);
+//     Bool  negate = FALSE;
+//     if (token.tag == TOKEN_MINUS) {
+//         negate = TRUE;
+//         token = pop_token(memory);
+//     }
+//     if (token.tag != TOKEN_NUMBER) {
+//         UNEXPECTED_TOKEN("get_signed", token);
+//     }
+//     if (negate) {
+//         return (i32)token.number * -1;
+//     }
+//     return (i32)token.number;
+// }
+
+static void set_major_version(Memory* memory) {
+    Token token = pop_token(memory);
+    if (token.tag != TOKEN_MAJOR_VERSION) {
+        UNEXPECTED_TOKEN("set_major_version", token);
+    }
+    memory->program.major_version = (u16)get_unsigned(memory);
+}
+
+static void set_minor_version(Memory* memory) {
+    Token token = pop_token(memory);
+    if (token.tag != TOKEN_MINOR_VERSION) {
+        UNEXPECTED_TOKEN("set_minor_version", token);
+    }
+    memory->program.minor_version = (u16)get_unsigned(memory);
+}
+
+static void set_program(Memory* memory) {
+    set_major_version(memory);
+    set_minor_version(memory);
 }
 
 i32 main(i32 n, const char** args) {
@@ -219,8 +287,15 @@ i32 main(i32 n, const char** args) {
     Memory* memory = calloc(1, sizeof(Memory));
     set_file_to_chars(memory, args[1]);
     set_tokens(memory);
-    print_tokens(memory);
-    printf("\nmemory->file_size : %u\nDone!\n", memory->file_size);
+    set_program(memory);
+    printf("\n"
+           "memory->file_size             : %u\n"
+           "memory->program.major_version : %hu\n"
+           "memory->program.minor_version : %hu\n"
+           "Done!\n",
+           memory->file_size,
+           memory->program.major_version,
+           memory->program.minor_version);
     free(memory);
     return EXIT_SUCCESS;
 }
